8088da2c165d998610f2d326c6827202926983d3
Gordon <gordon@codeplay.com>
2015-10-13 23:54:20 +0100

    Add sample code for blog.
    * introduction_to_template/instantiation.

---
+++ blog/introduction_to_templates/instantiation/instantiation.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+/* Function template that defines a generic groups of functions that take two
+ * reference parameters of the same type and swaps their values. */
+template <typename T> void swap(T &a, T &b) {
+  T tmp = a;
+  a = b;
+  b = tmp;
+}
+
+/* Simple program which calls the function template swap and triggers implicit
+ * instantiation. */
+int main() {
+  int a = 2, b = 5;
+  
+	std::cout << "before" << std::endl;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "a: " << b << std::endl;
+
+	swap(a, b);
+
+	std::cout << "after" << std::endl;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "a: " << b << std::endl;
+}
